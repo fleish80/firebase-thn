c8af8cd846e8c6f19d3a82d655fb61639fb90bf5
fleish80 <Fleish.D@gmail.com>
2018-10-17 18:41:39 +0300

    Firebase

---
+++ app.js
@@ -0,0 +1,79 @@
+const cafeList = document.querySelector('#cafe-list');
+const form = document.querySelector('#add-cafe-form');
+
+// create elements and ender cafe
+function renderCafe(doc) {
+    let li = document.createElement('li');
+    let name = document.createElement('span');
+    let city = document.createElement('span');
+    let cross = document.createElement('div');
+    
+    li.setAttribute('data-id', doc.id);
+    name.textContent = doc.data().name;
+    city.textContent = doc.data().city;
+    cross.textContent = 'x';
+    cross.setAttribute('tabindex', 0);
+
+    li.appendChild(name);
+    li.appendChild(city);
+    li.appendChild(cross);
+
+    cafeList.appendChild(li);
+
+    cross.addEventListener('click', function(e) {
+        e.stopPropagation();
+        let id = e.target.parentElement.getAttribute('data-id');
+        db.collection('cafes').doc(id).delete();    
+    });
+
+}
+
+//getting data
+// db.collection('cafes').orderBy('name').get().then((snapshot) => {
+//     snapshot.docs.forEach(doc => {
+//         renderCafe(doc);        
+//     });
+// });
+
+// getting data with where
+// db.collection('cafes').where('city', '==', 'Marioland').get().then((snapshot) => {
+//     snapshot.docs.forEach(doc => {
+//         renderCafe(doc);        
+//     });
+// });
+
+
+// saving data
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    db.collection('cafes').add({
+        name: form.name.value,
+        city: form.city.value
+    });
+    form.name.value = '';
+    form.city.value = '';
+});
+
+// get data with real time listener
+db.collection('cafes').orderBy('city').onSnapshot(snapshot => {
+    let changes = snapshot.docChanges();
+    changes.forEach(change =>{
+        if (change.type == 'added') {
+            renderCafe(change.doc)
+        } else if (change.type == 'removed') {
+            let li = cafeList.querySelector('[data-id=' + change.doc.id + ']');
+            cafeList.removeChild(li);
+        }
+
+    });
+});
+
+// // update
+// db.collection('cafes').doc('UTejQDtXKXdToLUt6Tgs').update({
+//     city: 'New York'
+// });
+
+// // ovverides whole document, deletes previous data
+// db.collection('cafes').doc('UTejQDtXKXdToLUt6Tgs').set({
+//     city: 'Liverpool'
+// })
